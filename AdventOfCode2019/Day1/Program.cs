9adf07e34e76adb7c1a07fe3ba3b19e41676efda
Jente Vets <jentevets@gmail.com>
2019-12-01 14:34:08 +0100

    Day 1 - Part 2

--- AdventOfCode2019/Day1/Program.cs
+++ AdventOfCode2019/Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Day1
@@ -11,7 +12,21 @@ namespace Day1
 			Console.WriteLine("");
 			Console.WriteLine("Started reading input...");
 			var modulesMass = System.IO.File.ReadAllLines(@"Input/input.txt");
-			var totalFuel = modulesMass.Select(m => Math.Floor((double)Convert.ToInt32(m) / 3) - 2).Sum();
+			var fuelList = new List<double>();
+			foreach (var module in modulesMass)
+			{
+				var fuelForModuleList = new List<double>();
+				var fuel = Math.Floor((double)Convert.ToInt32(module) / 3) - 2;
+				fuelForModuleList.Add(fuel);
+				while (fuel > 5)
+				{
+					fuel = Math.Floor(fuel / 3) - 2;
+					fuelForModuleList.Add(fuel);
+				}
+				fuelList.Add(fuelForModuleList.Sum());
+			}
+
+			var totalFuel = fuelList.Sum();
 			Console.WriteLine(totalFuel);
 			Console.ReadLine();
 		}
