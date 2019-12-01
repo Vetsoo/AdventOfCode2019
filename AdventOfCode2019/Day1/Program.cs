22897b03f9bf1c2215a9bbf8854340b2fc711002
Jente Vets <jentevets@gmail.com>
2019-12-01 14:26:55 +0100

    Day 1 - Part 1

---
+++ AdventOfCode2019/Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Day1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the DAY 1!");
+			Console.WriteLine("");
+			Console.WriteLine("Started reading input...");
+			var modulesMass = System.IO.File.ReadAllLines(@"Input/input.txt");
+			var totalFuel = modulesMass.Select(m => Math.Floor((double)Convert.ToInt32(m) / 3) - 2).Sum();
+			Console.WriteLine(totalFuel);
+			Console.ReadLine();
+		}
+	}
+}
