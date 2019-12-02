a5772f4f81ef8b34d633c348e1d9812d610d2097
Jente Vets <jente.vets@brusselsairlines.com>
2019-12-02 10:01:26 +0100

    Day 2 - Part 2

--- AdventOfCode2019/Day2/Program.cs
+++ AdventOfCode2019/Day2/Program.cs
@@ -9,26 +9,33 @@ namespace Day2
 			Console.WriteLine("Welcome to the DAY 2!");
 			Console.WriteLine("");
 			Console.WriteLine("Started reading input...");
-			var intCode = System.IO.File.ReadAllText(@"Input/input.txt");
-			var intCodeNumbers = intCode.Split(',');
-			intCodeNumbers[1] = "12";
-			intCodeNumbers[2] = "2";
-			var step = 0;
-			var code = Convert.ToInt32(intCodeNumbers[step]);
-			while (code != 99)
+			for (var x = 0; x < 100; x++)
 			{
-				if (code == 1)
+				for (var y = 0; y < 100; y++)
 				{
-					intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 3])] = (Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 1])]) + Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 2])])).ToString();
+					var intCode = System.IO.File.ReadAllText(@"Input/input.txt");
+					var intCodeNumbers = intCode.Split(',');
+					intCodeNumbers[1] = x.ToString();
+					intCodeNumbers[2] = y.ToString();
+					var step = 0;
+					var code = Convert.ToInt32(intCodeNumbers[step]);
+					while (code != 99)
+					{
+						if (code == 1)
+						{
+							intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 3])] = (Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 1])]) + Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 2])])).ToString();
+						}
+						if (code == 2)
+						{
+							intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 3])] = (Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 1])]) * Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 2])])).ToString();
+						}
+						step += 4;
+						code = Convert.ToInt32(intCodeNumbers[step]);
+					}
+					if (intCodeNumbers[0] == "19690720")
+						Console.WriteLine(100 * x + y);
 				}
-				if (code == 2)
-				{
-					intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 3])] = (Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 1])]) * Convert.ToInt32(intCodeNumbers[Convert.ToInt32(intCodeNumbers[step + 2])])).ToString();
-				}
-				step += 4;
-				code = Convert.ToInt32(intCodeNumbers[step]);
 			}
-			Console.WriteLine(intCodeNumbers[0]);
 			Console.ReadLine();
 		}
 	}
