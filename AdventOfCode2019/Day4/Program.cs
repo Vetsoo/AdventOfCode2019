8c341bca01591038e92b0062886f1b90e87e42ed
Jente Vets <jente.vets@brusselsairlines.com>
2019-12-04 08:07:02 +0100

    Day 4 - Part 1

---
+++ AdventOfCode2019/Day4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the DAY 4!");
+			Console.WriteLine("");
+			Console.WriteLine("Started reading input...");
+			var startInput = 271973;
+			var endInput = 785961;
+			var correctCounter = 0;
+			for (var i = startInput; i <= endInput; i++)
+			{
+				var doubleDigit = false;
+				var neverDecrease = true;
+				var lastNumber = 0;
+				var seperateNumbers = i.ToString().ToCharArray();
+				foreach (var number in seperateNumbers)
+				{
+					var n = Convert.ToInt32(number);
+					if (lastNumber == n)
+						doubleDigit = true;
+
+					if (n < lastNumber)
+						neverDecrease = false;
+
+					lastNumber = n;
+				}
+
+				if (doubleDigit && neverDecrease)
+					correctCounter++;
+			}
+
+			Console.WriteLine(correctCounter);
+		}
+	}
+}
