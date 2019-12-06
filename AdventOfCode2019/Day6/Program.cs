d494a510bfd963078ae7bf6e08b58a5bc735810f
Jente Vets <jente.vets@brusselsairlines.com>
2019-12-06 14:36:22 +0100

    Day 6 - Part 1

---
+++ AdventOfCode2019/Day6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the DAY 6!");
+			Console.WriteLine("");
+			Console.WriteLine("Started reading input...");
+			var directOrbits = System.IO.File.ReadAllLines(@"Input/input.txt");
+			var directOrbitsDictionary = directOrbits.Select(f => { var split = f.Split(')'); return new Tuple<string, string>(split[0], split[1]); });
+
+			var totalOrbitCounter = 0;
+			foreach (var directOrbit in directOrbitsDictionary)
+			{
+				var placeholder = directOrbit;
+				totalOrbitCounter++;
+				while (placeholder.Item1 != "COM")
+				{
+					var indirectOrbit = directOrbitsDictionary.First(o => o.Item2 == placeholder.Item1);
+					placeholder = indirectOrbit;
+					totalOrbitCounter++;
+				}
+			}
+			Console.WriteLine(totalOrbitCounter);
+			Console.ReadLine();
+		}
+	}
+}
