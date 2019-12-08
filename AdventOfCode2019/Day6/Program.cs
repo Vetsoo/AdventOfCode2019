0555df2b85eeae0d01db5602c4516c55d658efe5
Jente Vets <jentevets@gmail.com>
2019-12-08 16:33:18 +0100

    Day 6 - Part 2

--- AdventOfCode2019/Day6/Program.cs
+++ AdventOfCode2019/Day6/Program.cs
@@ -26,7 +26,59 @@ namespace Day6
 					totalOrbitCounter++;
 				}
 			}
+
 			Console.WriteLine(totalOrbitCounter);
+
+			// find the orbits from YOU
+			var you = directOrbitsDictionary.FirstOrDefault(f => f.Item2 == "YOU");
+			var allYouOrbits = new List<Tuple<string, string>>() { you };
+			while (you.Item1 != "COM")
+			{
+				var indirectOrbit = directOrbitsDictionary.First(o => o.Item2 == you.Item1);
+				you = indirectOrbit;
+				allYouOrbits.Add(you);
+			}
+
+			// find the orbits from SAN
+			var san = directOrbitsDictionary.FirstOrDefault(f => f.Item2 == "SAN");
+			var allSanOrbits = new List<Tuple<string, string>>() { san };
+			while (san.Item1 != "COM")
+			{
+				var indirectOrbit = directOrbitsDictionary.First(o => o.Item2 == san.Item1);
+				san = indirectOrbit;
+				allSanOrbits.Add(san);
+			}
+
+			var notIncommonYou = allYouOrbits.Except(allSanOrbits);
+			var notIncommonSan = allSanOrbits.Except(allYouOrbits);
+			var notincommon = new List<Tuple<string, string>>();
+			notincommon.AddRange(notIncommonYou);
+			notincommon.AddRange(notIncommonSan);
+			var planet = notincommon.Select(f => f.Item1).GroupBy(f => f).Where(f => f.Count() > 1).Select(f => f.Key).ToList();
+
+			var youEnd = allYouOrbits.FirstOrDefault(f => f.Item1 == planet.FirstOrDefault());
+			var sanEnd = allSanOrbits.FirstOrDefault(f => f.Item1 == planet.FirstOrDefault());
+
+			// find the orbits from YOU
+			you = directOrbitsDictionary.FirstOrDefault(f => f.Item2 == "YOU");
+			var totalYouOrbits = 1;
+			while (you.Item1 != youEnd.Item1)
+			{
+				var indirectOrbit = directOrbitsDictionary.First(o => o.Item2 == you.Item1);
+				you = indirectOrbit;
+				totalYouOrbits++;
+			}
+
+			// find the orbits from SAN
+			san = directOrbitsDictionary.FirstOrDefault(f => f.Item2 == "SAN");
+			var totalSanOrbits = 1;
+			while (san.Item1 != youEnd.Item1)
+			{
+				var indirectOrbit = directOrbitsDictionary.First(o => o.Item2 == san.Item1);
+				san = indirectOrbit;
+				totalSanOrbits++;
+			}
+			Console.WriteLine(totalYouOrbits + totalSanOrbits - 2);
 			Console.ReadLine();
 		}
 	}
